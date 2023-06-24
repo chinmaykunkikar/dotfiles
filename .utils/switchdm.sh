952550fa2fa2bf7c54e8ded74164f11084296673
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-06-24 19:59:23 +0530

    Update switchdm.sh

--- .utils/switchdm.sh
+++ .utils/switchdm.sh
@@ -2,25 +2,22 @@
 
 # Function to switch to TTY
 switch_to_tty() {
-  echo "Switching to TTY..."
+  local tty_number
+  tty_number=$(basename "$(tty)" | sed 's/tty//')
+  echo "Switching to TTY${tty_number}..."
   sudo systemctl stop display-manager.service
-  sudo systemctl start getty@tty1.service
-  echo "Switched to TTY"
+  sudo systemctl start getty@tty${tty_number}.service
+  sudo chvt "${tty_number}"
+  echo "Switched to TTY${tty_number}"
 }
 
 # Function to switch to the display manager
 switch_to_display_manager() {
-  local display_manager=$(systemctl show display-manager.service --property=Description --value)
-
-  if [[ -z "$display_manager" ]]; then
-    echo "Failed to retrieve the display manager name. Aborting..."
-    exit 1
-  fi
-
-  echo "Switching to $display_manager..."
+  echo "Switching to display manager..."
   sudo systemctl stop getty@tty1.service
   sudo systemctl start display-manager.service
-  echo "Switched to $display_manager"
+  sudo chvt 1
+  echo "Switched to display manager"
 }
 
 # Check if systemctl command is available
@@ -30,12 +27,18 @@ if ! command -v systemctl >/dev/null 2>&1; then
   exit 1
 fi
 
+# Check if current session is a TTY
+if [[ $(tty) =~ /dev/tty([0-9]+) ]]; then
+  current_tty=${BASH_REMATCH[1]}
+  echo "Already in a TTY session (TTY${current_tty}). Switching to the display manager..."
+  #switch_to_display_manager
+  exit 0
+fi
+
 # Check current display manager and toggle accordingly
 current_manager=$(systemctl show display-manager.service --property=ActiveState --value)
 if [[ "$current_manager" == "active" ]]; then
-  echo "Current display manager: $current_manager"
   switch_to_tty
 else
-  echo "Current display manager: $current_manager"
   switch_to_display_manager
 fi
