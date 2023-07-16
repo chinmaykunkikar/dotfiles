447978bba5c1c55b675ad34dd664e5b52c2a38d0
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-07-16 17:25:21 +0530

    bash: Add support ntfy notification

--- bash/.local/battery_health_notification.sh
+++ bash/.local/battery_health_notification.sh
@@ -14,15 +14,20 @@ LOW_THRESHOLD=20
 
 # Define some values about host and remote machines
 HOSTNAME=$(hostname)
-NEPTUNE_IP='192.168.0.122'
+REMOTE_IP='192.168.0.122'
 REMOTE_USERNAME='chinmay'
+NFTY_IP="192.168.0.123:6839"
+NFTY_TOPIC="battery_alerts"
 
 # Define the summary and body messages
-BATT_LOW_SUMMARY="Battery Low ($BATTERY_PERCENTAGE% remaining)"
-BATT_LOW_BODY="Battery is running low on your <i>$HOSTNAME</i> server. Plug it in to avoid disruption of services."
+DESKTOP_SUMMARY="Battery Low ($BATTERY_PERCENTAGE% remaining)"
+DESKTOP_BODY="Battery is running low on your <i>$HOSTNAME</i> server. Plug it in to avoid disruption of services."
+NTFY_TITLE="Battery Low ($BATTERY_PERCENTAGE% remaining)"
+NTFY_BODY="$HOSTNAME server is low on battery. Plug it in to avoid disruption of services."
 
 # Check if the battery is unplugged and charge is lower than threshold
 if [[ $CABLE_PLUGGED == 'no' && $BATTERY_PERCENTAGE -le $LOW_THRESHOLD ]]; then
     # Send low battery notification to remote machine
-    ssh "$REMOTE_USERNAME@$NEPTUNE_IP" "DISPLAY=:0 notify-send --urgency=critical --expire-time=0 --icon=battery-caution --app-name=Supernova \"$BATT_LOW_SUMMARY\" \"$BATT_LOW_BODY\""
+    curl -H "Title: $NTFY_TITLE" -H "Priority: urgent" -H "Tags: exclamation, battery, $HOSTNAME" -d "$NTFY_BODY" "$NFTY_IP"/"$NFTY_TOPIC"
+    ssh "$REMOTE_USERNAME@$REMOTE_IP" "DISPLAY=:0 notify-send --urgency=critical --expire-time=0 --replace-id=8484 --icon=battery-caution --app-name=Supernova \"$DESKTOP_SUMMARY\" \"$DESKTOP_BODY\""
 fi
